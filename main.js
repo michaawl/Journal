4932464705c405b4f6fc3e865dc261efb1964bff
michaawl <117541955+michaawl@users.noreply.github.com>
2024-11-13 22:50:50 +0100

    created react template

--- main.js
+++ main.js
@@ -7,16 +7,19 @@ function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Journal',
         width: '1000',
-        height: '600'
+        height: '600',
+
     });
 
+    mainWindow.webContents.openDevTools();
+
     const startUrl = url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, './app/build/index.html'),
 
         protocol: 'file',
     })
 
     mainWindow.loadURL(startUrl);
-}
+}    
 
 app.whenReady().then(createMainWindow)
